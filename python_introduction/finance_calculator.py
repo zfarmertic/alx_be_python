c0b2594e39efa547a74fc5006a85e37c53e01020
zfarmertic <lilyigbunu@gmail.com>
2025-11-05 10:01:58 +0100

    factorial correxted

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,11 +1,12 @@
 monthly_income = int(input("Enter your monthly income: "))
-total_monthly_expenses = int(input("Enter your total monthly expenses: "))
+
+monthly_expenses = int(input("Enter your total monthly expenses: "))
 
 
-monthly_savings = int(monthly_income - (total_monthly_expenses))
+monthly_savings = int(monthly_income - (monthly_expenses))
 
-annual_savings = int((monthly_savings * 12) + (monthly_savings * 12 * 0.05))
+projected_savings = int(monthly_savings * 12 + (monthly_savings * 12 * 0.05))
 
 print(f"Your monthly savings are ${monthly_savings}.")
-print(f"Projected savings after one year, with interest, is: ${annual_savings}.")
+print(f"Projected savings after one year, with interest, is: ${projected_savings}.")
 
