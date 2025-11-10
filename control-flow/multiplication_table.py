2f4b418829fa1855259b597b9f0e405c798e3727
zfarmertic <lilyigbunu@gmail.com>
2025-11-10 19:29:25 +0100

    multiply

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input("Enter a number to see its multiplication table: "))
-for nums in range(10):
+for nums in range(1, 10):
     Z = nums * number
     print(f"{nums} * {number} = {Z}")
