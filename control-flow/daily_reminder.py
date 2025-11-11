fa82958f756ac4978d6ae76605c6901f21396b0b
zfarmertic <lilyigbunu@gmail.com>
2025-11-12 00:17:05 +0100

    task

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,14 +1,26 @@
 task = input("Enter your task: ")
-Priority = input("Priority (high, medium, low): ")
+priority = input("Priority (high, medium, low): ")
 time_bound = input("Is it time-bound? (yes/no): ")
 
-match Priority:
-    case "high":
-        
-    case "meduim":
-        
-    case "low":
-        
+is_time_bound = " "
+priority_msg = " "
+
+match priority:
+    case 'high':
+        priority_msg = f"Task {task} is {priority} priority"
+    case 'medium':
+        priority_msg = f"Task {task} is {priority} priority"
+    case 'low':
+        priority_msg = f"Task {task} is {priority} priority"
     case _:
-        print("")
-        
+        priority_msg = f"Task {task} is unrecognized"
+
+if time_bound == "yes":
+    is_time_bound = " that requires immediate attention today"
+elif time_bound == "no":
+    is_time_bound = ", which can be scheduled later this week"
+else:
+    is_time_bound = ", but the time-sensitivity is unclear"
+
+
+print(f"{priority_msg} {is_time_bound}")
