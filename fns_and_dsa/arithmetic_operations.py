6a24026cdcdf21bd1baf2ffd2d49da42fa67d787
zfarmertic <lilyigbunu@gmail.com>
2025-12-04 21:00:34 +0100

    operation file added

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation(num1:float, num2:float, operation:str):
+def perform_operation(num1, num2, operation):
     if operation == 'add':
         return num1 + num2
     elif operation == 'subtract':
