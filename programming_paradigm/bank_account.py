97873e69f1f6e3b6f7c909406189364c70437d6d
zfarmertic <lilyigbunu@gmail.com>
2025-12-05 16:42:13 +0100

    bank account file

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -19,4 +19,4 @@ class BankAccount:
             return False
         
     def display_balance(self):
-        print(f"Current Balance: ${self.account_balance}")
+        print(f"Current Balance: ${self.account_balance:.2f}")
