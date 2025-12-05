9bac8c35ea645df3e76f7ed2b402a45c9897395c
zfarmertic <lilyigbunu@gmail.com>
2025-12-05 16:59:57 +0100

    error handled

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -7,7 +7,7 @@ def safe_divide(numerator, denominator):
     except ZeroDivisionError:
         return "Error: Cannot divide by zero."
     except ValueError:
-        return "Error: Please enter numeric values only"
+        return "Error: Please enter numeric values only."
     # else:
     #     return nume/deno
     # finally:
