f08ccbd00fd4865028d56d12c4561449a55c2c2d
zfarmertic <lilyigbunu@gmail.com>
2025-12-27 16:45:17 +0100

    uninit test

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,7 +5,7 @@ class TestCalc(unnittest.TestCase):
     def calc(self):
         self.calc = SimpleCalculator()
     
-    def test_add(self):
+    def test_addition(self):
         self.assertEqual(self.calc.add(2, 3), 5)
         self.assertEqual(self.calc.add(-1, 1), 0)
     
