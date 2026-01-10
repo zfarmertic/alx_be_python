12af822f2dfbfec3b6d05b527bd27d741a3bee3a
zfarmertic <lilyigbunu@gmail.com>
2026-01-10 17:17:11 +0100

    added book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -4,7 +4,7 @@ class Book:
         self.author = author
         self.year = year
     def __del__(self):
-        print("Deleting (title of the book")
+        print(f"Deleting {self.title}")
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
     
