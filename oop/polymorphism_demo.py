2a7af3c280c22a9a5843a62505e7ea73e926d17b
zfarmertic <lilyigbunu@gmail.com>
2026-01-10 18:06:41 +0100

    polymorphism tested

---
+++ oop/polymorphism_demo.py
@@ -0,0 +1,24 @@
+import math
+
+class Shape:
+    def area(self):
+        return NotImplementedError("derived classes muct overrie this error")
+    
+
+class Rectangle(Shape):
+    def __init__(self, length, width):
+        self.length = length
+        self.width = width
+
+    def area(self):
+        return self.length * self.width
+    
+
+class Circle(Shape):
+    def __init__(self, radius):
+        self.radius = radius
+
+    def area(self):
+        return math.pi * (self.radius **2)
+    
+
