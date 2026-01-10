ee590d7db8659fbc8de1d91e59ad40f6d4cb2fc9
zfarmertic <lilyigbunu@gmail.com>
2026-01-10 17:15:36 +0100

    added book class

--- oop/book_class.py
+++ oop/book_class.py
@@ -6,6 +6,9 @@ class Book:
     def __del__(self):
         print("Deleting (title of the book")
     def __str__(self):
-        return "title by author, published in year"
+        return f"{self.title} by {self.author}, published in {self.year}"
+    
     def __repr__(self):
-        return f"{self.title} by {self.author} published in {self.year}"
+        return f"Book('{self.title}', '{self.author}', {self.year})"
+    
+    
